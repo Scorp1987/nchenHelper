e78996389abfb4b35874fa510343c59972e64ad1
Nathan Chen <nchen1@illumina.com>
2022-03-30 13:49:04 +0800

    Add nchenTelegram

---
+++ nchenTelegram/Enums/TextFormat.cs
@@ -0,0 +1,12 @@
+﻿namespace Telegram.Bot.Enums
+{
+    public enum TextFormat
+    {
+        Regular = 0,
+        Bold = 1,
+        Italic = 2,
+        Underline = 4,
+        Strikethrough = 8,
+        Spoiler = 16,
+    }
+}
